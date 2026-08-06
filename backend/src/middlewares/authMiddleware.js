0839df56d8163be35b29523398359930792d1023
IsaacGtzGz <isaacgtzgz@gmail.com>
2026-08-06 15:08:13 -0600

    feat(backend): middleware de autenticación con JWT

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
+    }
+
+    try {
+        const verified = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Token inválido o expirado' });
+    }
+};
+
+module.exports = {
+    verifyToken
+};
