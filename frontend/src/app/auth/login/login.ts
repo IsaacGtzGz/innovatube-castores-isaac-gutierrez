579352b3510f1c8ceb935f652f175dc69b037a62
IsaacGtzGz <isaacgtzgz@gmail.com>
2026-08-07 13:49:00 -0600

    feat(auth): validacion de credenciales con Reactive Forms en Login

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Payload listo para la API:', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
