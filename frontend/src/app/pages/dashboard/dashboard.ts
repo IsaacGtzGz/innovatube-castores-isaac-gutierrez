fb234a0a5265283d132bb40fb358f10894a52318
IsaacGtzGz <isaacgtzgz@gmail.com>
2026-08-07 22:10:19 -0600

    feat(dashboard): implementar reproductor de video con modal y dom sanitizer

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,36 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 // Servicios
 import { VideoService } from '../../services/video';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class DashboardComponent implements OnInit {
   videos: any[] = [];
+  searchInput = new FormControl('');
+
+  // Variables del Modal
+  videoSeleccionado: any = null;
+  urlSegura: SafeResourceUrl | null = null;
 
   // Inyección de Dependencias
   constructor(
     private router: Router,
-    private videoService: VideoService
+    private videoService: VideoService,
+    private sanitizer: DomSanitizer
   ) { }
 
   // Ciclo de Vida
   ngOnInit(): void {
-    this.cargarVideos();
+    this.cargarVideos('desarrollo web');
+  }
+
+  // Eventos de Interfaz
+  buscar(): void {
+    const termino = this.searchInput.value;
+    if (termino && termino.trim() !== '') {
+      this.cargarVideos(termino);
+    }
+  }
+
+  limpiarBusqueda(): void {
+    this.searchInput.setValue('');
+    this.cargarVideos('desarrollo web');
+  }
+
+  // Lógica del Reproductor
+  abrirVideo(video: any): void {
+    this.videoSeleccionado = video;
+    // Armamos la URL de incrustación de YouTube y la sanitizamos
+    const url = `https://www.youtube.com/embed/${video.youtube_video_id}?autoplay=1`;
+    this.urlSegura = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  cerrarModal(): void {
+    this.videoSeleccionado = null;
+    this.urlSegura = null;
   }
 
   // Peticiones HTTP
-  cargarVideos(): void {
-    // Llamamos a la nueva función searchVideos
-    this.videoService.searchVideos('desarrollo web').subscribe({
+  cargarVideos(query: string): void {
+    this.videos = [];
+
+    this.videoService.searchVideos(query).subscribe({
       next: (data) => {
         this.videos = data;
-        console.log('Respuesta de la API de YouTube:', this.videos);
       },
       error: (err) => {
         console.error('Error al cargar la galería:', err);
